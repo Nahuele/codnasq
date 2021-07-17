0decb7d28b0b2de4e0d64856f86706e803fee75d
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-07-17 14:45:41 -0500

    PDB search fixed

--- app/controllers/cluster_controller.rb
+++ app/controllers/cluster_controller.rb
@@ -1,16 +1,23 @@
 class ClusterController < ApplicationController
   def show
-    cluster = Cluster.find_by_codnasq_id(params[:codnasq_id]) || Cluster.find(cluster_id)
-    conformers_cluster = Conformer.where(cluster: cluster)
-
-    render 'cluster/show',
-           locals: {
-             cluster: cluster,
-             conformers: conformers_cluster,
-             max_pair: cluster.max_rmsd_pair,
-             query: cluster.max_rmsd_pair.query,
-             target: cluster.max_rmsd_pair.target,
-           }
+    begin
+      cluster = Conformer.find_by_pdb_id([params[:codnasq_id]]) || Cluster.find(cluster_id)
+      if cluster.class.to_s == 'Conformer'
+        cluster = Cluster.find_by_codnasq_id(cluster.cluster_id)    
+      end     
+      conformers_cluster = Conformer.where(cluster: cluster)
+      render 'cluster/show',
+        locals: {
+          cluster: cluster,
+          conformers: conformers_cluster,
+          max_pair: cluster.max_rmsd_pair,
+          query: cluster.max_rmsd_pair.query,
+          target: cluster.max_rmsd_pair.target,
+        }
+    rescue => e
+      # Redirect to not-found page
+      redirect_to root_path
+    end
   end
 
   def search
@@ -21,7 +28,7 @@ class ClusterController < ApplicationController
       @conformers = Conformer.where('lower(name) LIKE ?', "%#{query}%").page(params[:page])
     when 'Organism'
       @conformers = Conformer.where('lower(organism) LIKE ?', "%#{query}%").page(params[:page])
-    when 'PDB'
+    when "Cluster (by PDB)"
       redirect_to cluster_show_path(query)
     else
       @conformers = Conformer.search_in_all_fields(query).page(params[:page])
